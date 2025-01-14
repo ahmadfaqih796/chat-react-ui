c1bfc98107039532cad7a5d48ea11435d5acda58
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-01-14 11:55:59 +0700

    commit

---
+++ src/reportWebVitals.js
@@ -0,0 +1,22 @@
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      console.log(
+        "xxxxxxxxxx",
+        getCLS,
+        getFID,
+        getFCP,
+        getLCP,
+        getTTFB,
+        onPerfEntry
+      );
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
