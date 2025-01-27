c89a2fe1e9d68709f49d6818bb48f8f99ca0ac69
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-01-28 01:47:47 +0700

    search user list

--- src/pages/chat/components/ChatList.jsx
+++ src/pages/chat/components/ChatList.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { USER_LIST } from "./userDummy";
+import { Box, TextField } from "@mui/material";
 
 const ChatList = () => {
+  const [search, setSearch] = React.useState("");
   const data = USER_LIST;
+
+  const filterData = data.filter((item) => {
+    return item.name.toLowerCase().includes(search.toLowerCase());
+  });
   return (
-    <div>
-      {data.map((item) => (
+    <Box
+      sx={{
+        p: 2,
+      }}
+    >
+      <TextField
+        id="outlined-basic"
+        label="Pencarian"
+        variant="outlined"
+        size="small"
+        fullWidth
+        sx={{
+          mt: 1,
+          "& .MuiOutlinedInput-root": {
+            borderRadius: "10px",
+            overflow: "hidden",
+          },
+        }}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {filterData.map((item) => (
         <div key={item.id}>{item.name}</div>
       ))}
-    </div>
+    </Box>
   );
 };
 
