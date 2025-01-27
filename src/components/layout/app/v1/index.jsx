68cdc0de4fd2dc94ab04a6e660ada380aaa526e1
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-01-27 13:06:29 +0700

    Desain Mobile Chat

--- src/components/layout/app/v1/index.jsx
+++ src/components/layout/app/v1/index.jsx
@@ -1,9 +1,19 @@
-import { Box, Drawer } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import ChatLayout from "./Chat";
 import Sidebar from "./Sidebar";
 import Profile from "./Profile";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const WIDTH = {
   sidebar: "70px",
@@ -13,6 +23,9 @@ const WIDTH = {
 
 const BaseLayout = () => {
   const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
 
   const handleOpenDrawer = () => {
     setOpen(!open);
@@ -20,16 +33,41 @@ const BaseLayout = () => {
 
   return (
     <div>
+      {isMobile && (
+        <AppBar position="fixed">
+          <Toolbar
+            sx={{
+              height: "64px",
+            }}
+          >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              // onClick={toggleDrawer}
+              // className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Chat
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      )}
       <Box
         sx={{
+          pt: isMobile ? "64px" : "0px",
           display: "flex",
           width: "100vw",
           height: "100vh",
           bgcolor: "#bae0ff",
+          overflowX: "auto",
         }}
       >
         <Box
           sx={{
+            display: isMobile ? "none" : "block",
             width: WIDTH.sidebar,
             p: 1,
           }}
@@ -38,7 +76,8 @@ const BaseLayout = () => {
         </Box>
         <Box
           sx={{
-            width: "350px",
+            width: isMobile ? "100vw" : "350px",
+            minWidth: isMobile ? "100%" : "350px",
             p: 1,
           }}
         >
@@ -46,9 +85,16 @@ const BaseLayout = () => {
         </Box>
         <Box
           sx={{
-            width: `calc(100% - ${WIDTH.sidebar} - ${WIDTH.content} - ${
-              open ? WIDTH.profile : "0px"
-            })`,
+            width: isMobile
+              ? "100%"
+              : `calc(100% - ${WIDTH.sidebar} - ${WIDTH.content} - ${
+                  open && isDesktop ? WIDTH.profile : "0px"
+                })`,
+            minWidth: isMobile
+              ? "100%"
+              : `calc(100% - ${WIDTH.sidebar} - ${WIDTH.content} - ${
+                  open && isDesktop ? WIDTH.profile : "0px"
+                })`,
             transition: "all 0.2s ease-in-out",
             p: 1,
           }}
@@ -60,13 +106,13 @@ const BaseLayout = () => {
               flexShrink: 0,
               "& .MuiDrawer-paper": {
                 border: "none",
-                background: "#bae0ff",
+                background: isDesktop ? "#bae0ff" : "#fff",
                 p: 1,
                 width: WIDTH.profile,
                 boxSizing: "border-box",
               },
             }}
-            variant="persistent"
+            variant={isDesktop ? "persistent" : "temporary"}
             anchor="right"
             open={open}
           >
