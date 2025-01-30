3db8476df53946e61c69e9b124424df378c6ac2b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-01-30 13:21:21 +0700

    login style

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useAlert } from "../../hooks/useAlert";
 import { useAuth } from "../../hooks/useAuth";
-import logo from "../../logo.svg";
+import "./auth.style.css";
+// import logo from "../../logo.svg";
 
 function LoginApp() {
+  const [isActive, setIsActive] = React.useState(false);
   const { onLogin } = useAuth();
   const { onAlert } = useAlert();
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
 
   const logIn = () => {
     const userData = {
@@ -21,11 +27,80 @@ function LoginApp() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={logIn}>Log in</button>
-      </header>
+    <div className={isActive ? "container active" : "container"} id="container">
+      {/* Sign Up */}
+      <div className="form-container sign-up">
+        <form action="">
+          <h1>Sign Up</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your email for registeration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      {/* Sign In */}
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your email password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forget Your Password?</a>
+          <button onClick={logIn}>Sign In</button>
+        </form>
+      </div>
+
+      {/* Toggle */}
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of site features</p>
+            <button className="hidden" id="login" onClick={handleClick}>
+              Sign In
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>
+              Register with your personal details to use all of site features
+            </p>
+            <button className="hidden" id="register" onClick={handleClick}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
